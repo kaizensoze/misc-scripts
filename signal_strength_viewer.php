20562625dbdde4c63d6c9ef65c771819928216ac
Joe Gallo <gallo.j@gmail.com>
2015-06-21 21:17:28 -0400

    remove vpn autoconnect app

--- signal_strength_viewer.php
+++ signal_strength_viewer.php
@@ -5,7 +5,7 @@ $iwconfig = '/usr/sbin/airport -I';
 while (1) { 
     $out = `$iwconfig`; 
     $outa = explode("\n", $out); 
-    list( $key, $value ) = explode(':', $outa[0]); 
+    list( $key, $value ) = explode(':', $outa[0]);
     print str_repeat('+', -1* $value)."\n"; 
     usleep(100000); 
 } 
